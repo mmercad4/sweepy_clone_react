3a20c7ccf0b07092f237d890a69a2e01116c943e
Marco Mercado <marco@stonepawtraining.com>
2023-12-20 12:00:52 -0500

    Added frequency to dummy data

--- src/components/RoomForm.jsx
+++ src/components/RoomForm.jsx
@@ -1,4 +1,12 @@
-export default function RoomForm({ submitNewRoom }) {
+import { useState } from "react";
+
+export default function RoomForm() {
+  const [cleanliness, setCleanliness] = useState(100);
+
+  const handleCleanlinessChange = (event) => {
+    setCleanliness(event.target.value);
+  };
+
   return (
     <div className="flex justify-center items-center">
       <div className="lg:w-2/5 md:w-1/2 w-2/3">
@@ -13,9 +21,9 @@ export default function RoomForm({ submitNewRoom }) {
             <input
               className="w-full bg-slate-700 px-4 py-2 rounded-lg focus:outline-none focus:text-white"
               type="text"
-              name="Room name"
-              id="Room name"
-              placeholder="Room name"
+              name="RoomName"
+              id="RoomName"
+              placeholder="RoomName"
             />
           </div>
           <h1 className="text-center text-2xl mb-6 mt-10 text-white font-bold font-sans">
@@ -35,14 +43,18 @@ export default function RoomForm({ submitNewRoom }) {
           </div>
           <div>
             <label className="text-white font-semibold block my-3 text-md">
-              Task name
+              Current Cleanliness: {cleanliness}
             </label>
             <input
-              className="w-full bg-slate-700 px-4 py-2 rounded-lg focus:outline-none focus:text-white"
-              type="text"
-              name="Room name"
-              id="Room name"
-              placeholder="Room name"
+              className="w-full appearance-none bg-slate-700 h-2 rounded-lg outline-none text-white"
+              type="range"
+              name="cleanliness"
+              id="cleanliness"
+              value={cleanliness}
+              onChange={handleCleanlinessChange}
+              placeholder="cleanliness"
+              min="1"
+              max="100"
             />
           </div>
           <button className="w-full mt-6 bg-blue-950 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">
